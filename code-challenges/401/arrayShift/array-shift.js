2fb918117903ca1e5c864f60f29f1f53a04cec73
Earl Jay Caoile <ecaoile@my.hpu.edu>
2020-01-11 13:42:56 -0800

    added remove mid function and fixed indent spacing

--- code-challenges/401/arrayShift/array-shift.js
+++ code-challenges/401/arrayShift/array-shift.js
@@ -1,26 +1,39 @@
 'use strict';
 
 const shiftArray = (arr, num) => {
-    if (!num && num !== 0) return arr;
+  if (!num && num !== 0) return arr;
 
-    if (!arr && num !== 0) return [num];
+  if (!arr && num !== 0) return [num];
 
-    if (arr.length < 1) {
-        arr[0] = num;
-        return arr;
-    }
-
-    const mid = Math.round(arr.length / 2);
-    let outside = num;
-
-    for (let i = mid; i < arr.length; i++) {
-        let inside = arr[i];
-        arr[i] = outside;
-        outside = inside;
-    }
-
-    arr.push(outside);
+  if (arr.length < 1) {
+    arr[0] = num;
     return arr;
-}
+  }
 
-module.exports = { shiftArray };
+  const mid = Math.round(arr.length / 2);
+  let outside = num;
+
+  for (let i = mid; i < arr.length; i++) {
+    let inside = arr[i];
+    arr[i] = outside;
+    outside = inside;
+  }
+
+  arr.push(outside);
+  return arr;
+};
+
+const removeMid = (arr) => {
+  if (!arr) return arr;
+
+  const mid = Math.round(arr.length / 2);
+
+  for (let i = mid; i < arr.length; i++) {
+    arr[i] = arr[i + 1];
+  }
+
+  arr.length--;
+  return arr;
+};
+
+module.exports = { shiftArray, removeMid };
--- code-challenges/401/arrayShift/array-shift.test.js
+++ code-challenges/401/arrayShift/array-shift.test.js
@@ -1,35 +1,35 @@
 const { shiftArray } = require('./array-shift.js');
 
 describe('shiftArray', () => {
-    it('can return a single element array even with a falsy array value', () => {
-        const falsyArrList = [ [], null, undefined ];
-        const num = 3;
-        falsyArrList.forEach(falsyArr => {
-            expect(shiftArray(falsyArr, num)).toEqual([num]);
-        });
+  it('can return a single element array even with a falsy array value', () => {
+    const falsyArrList = [[], null, undefined];
+    const num = 3;
+    falsyArrList.forEach(falsyArr => {
+      expect(shiftArray(falsyArr, num)).toEqual([num]);
     });
+  });
 
-    it('can return the original array even with a null or undefined num value', () => {
-        noNumList = [ null, undefined ];
-        
-        noNumList.forEach(falsyVal => {
-            const arr = [1, 2, 3];
-            expect(shiftArray(arr, falsyVal)).toEqual(arr);
-        });
-    });
-    
-    it('will add 0 to an array containing the value 0', () => {
-        expect(shiftArray([0], 0)).toEqual([0,0]);
-    });
+  it('can return the original array even with a null or undefined num value', () => {
+    const noNumList = [null, undefined];
 
-    it('can add a value to the middle of an array', () => {
-        const num = 0;
-        const evenLenArr = [1, 2, 3, 4];
-        const oddLenArr = [5, 7, 13];
-        const expectedArr1 = [1, 2, num, 3, 4];
-        const expectedArr2 = [5, 7, num, 13];
-
-        expect(shiftArray(evenLenArr, num)).toEqual(expectedArr1);
-        expect(shiftArray(oddLenArr, num)).toEqual(expectedArr2);
+    noNumList.forEach(falsyVal => {
+      const arr = [1, 2, 3];
+      expect(shiftArray(arr, falsyVal)).toEqual(arr);
     });
+  });
+
+  it('will add 0 to an array containing the value 0', () => {
+    expect(shiftArray([0], 0)).toEqual([0, 0]);
+  });
+
+  it('can add a value to the middle of an array', () => {
+    const num = 0;
+    const evenLenArr = [1, 2, 3, 4];
+    const oddLenArr = [5, 7, 13];
+    const expectedArr1 = [1, 2, num, 3, 4];
+    const expectedArr2 = [5, 7, num, 13];
+
+    expect(shiftArray(evenLenArr, num)).toEqual(expectedArr1);
+    expect(shiftArray(oddLenArr, num)).toEqual(expectedArr2);
+  });
 });
