f284325a0176b503619e260cf938c895c582bbef
Earl Jay Caoile <unfie555@gmail.com>
2020-01-26 13:18:11 -0800

    working LL implementation with insert, includes, toString, and tests

---
+++ data-structures/linkedList/linked-list.js
@@ -0,0 +1,35 @@
+'use strict';
+const Node = require('./node.js');
+
+class LinkedList {
+    constructor() {
+        this.head = null;
+    }
+
+    insert(value) {
+        const newNode = new Node(value);
+        newNode.next = this.head;
+        this.head = newNode;
+    }
+
+    includes(value) {
+        let current = this.head;
+        while (current !== null) {
+            if (current.value === value) { return true; }
+            current = current.next;
+        }
+        return false;
+    }
+
+    toString() {
+        let res = '';
+        let current = this.head;
+        while (current !== null) {
+            res += `${current.value} -> `;
+            current = current.next;
+        }
+        return res + 'NULL';
+    }
+}
+
+module.exports = LinkedList;
