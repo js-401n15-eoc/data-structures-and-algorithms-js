c2a4fa8a84c8ea43c87a6377953d36606b45a0ea
Earl Jay Caoile <unfie555@gmail.com>
2020-02-02 18:20:04 -0800

    got code from whiteboard written

---
+++ data-structures/linkedList/llMerge/ll-merge.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const mergeLists = (list1, list2) => {
+    let head1 = list1.head;
+    let head2 = list2.head;
+
+    if (!head1) return list2;
+    if (!head2) return list1;
+
+    let curr1 = list1.head;
+    let curr2 = list2.head;
+
+    while (curr1 && curr2) {
+        let tmp = curr1.next;
+        curr1.next = curr2;
+        curr1 = tmp;
+        curr2 = curr2.next;
+    }
+
+    if (curr2) { curr1.next = curr2; }
+
+    return list1;
+}
